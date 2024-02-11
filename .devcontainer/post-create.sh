73e89ba914ffcba1190bf99bfd3a9f45c154c5e2
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2024-02-11 23:41:37 +0000

    add git pull

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 cd $REDMINE_ROOT
 
+if [ -d .git.sv ]
+then
+    ln -s .git.sv .git
+    git pull
+    rm .
+fi
+
 ln -s /workspaces/${PLUGIN_NAME} plugins/${PLUGIN_NAME}
 if [ -f plugins/${PLUGIN_NAME}/Gemfile_for_test ]
 then
